30634cc58596fa64b4ca52b2ccb64714fa247b98
Joseatr <43467350+Joseatr@users.noreply.github.com>
2019-03-18 20:37:35 +0100

    Act 18/03 - 1
    Subiendo PHP

---
+++ LMSGI/Trabajos/PHP/Ejercicio.PHP.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        //EJ1
+        /*
+        $numero = 10;
+
+        if($numero%2 == 0){
+            echo 'PAR';
+        }else{
+            echo 'IMPAR';
+        }
+        print "<br>";
+        //EJ2
+
+        $num1 = 2;
+        $num2 = 4;
+
+        if(($num1+$num2) > ($num1*$num2)){
+            echo 'La suma es mayor que el producto';
+        }else{
+            echo 'El producto es mayor que la suma';
+        }
+        print "<br>";
+        //EJ3
+
+        $v1 = 1;
+        $v2 = 2;
+        $nombre = 'Jose';
+        $apellido = 'Tirado Roman';
+
+        if($v1 > 0 && $v2 < $v1){
+            echo $nombre;
+        }else if($v1 > 0 && $v2 > $v1){
+            echo $apellido;
+        }else{
+            echo $nombre + ' ' + $apellido;
+        }
+        print "<br>";
+        
+        //EJ4
+
+        $variable = 4;
+
+        for($i = 1; $i <= 10; $i++){
+            echo $variable . ' * ' . $i . ' = ' . ($variable*$i);
+            print "<br>";
+        }
+        
+        //EJ5
+
+        for($i = 100; $i >= 0; $i -= 2){
+            echo $i. "<br>";
+        }
+        
+        //EJ6
+
+        for($i = 0; $i < 7; $i++){
+            print "<br>";
+            for($j = 0; $j < $i; $j++){
+                echo '*';
+            }
+        }
+        */
+        //EJ7
+        
+
+    ?>
+
+</body>
+</html>
